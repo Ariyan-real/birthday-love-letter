25bdfa7415441544bcfba9739c03cdb267e66767
Ariyan-real <azoainariyan2006@gmail.com>
2026-06-17 14:30:55 +0600

    Add files via upload

---
+++ Birthday letter 2/script.js
@@ -0,0 +1,25 @@
+function openLetter(){
+document.getElementById("letter").style.display="block";
+}
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML="❤️";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.fontSize=
+Math.random()*20+15+"px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},8000);
+}
+
+setInterval(createHeart,300);
